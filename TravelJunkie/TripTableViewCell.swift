f13e5170783ca92c4010760068284729ea067705
NerdboyTyler <tweppler44@yahoo.com>
2016-11-18 20:23:39 -0600

    Trips

---
+++ TravelJunkie/TripTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TripTableViewCell.swift
+//  TravelJunkie
+//
+//  Created by Tyler Weppler on 11/18/16.
+//  Copyright © 2016 Tyler Weppler. All rights reserved.
+//
+
+import UIKit
+
+class TripTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tripTitleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
