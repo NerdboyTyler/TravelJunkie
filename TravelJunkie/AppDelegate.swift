1c9e52f67bee9e05a85230b7e626f7cfa29d81b8
Ammar Hassan <ammarhz.7j@gmail.com>
2016-11-21 19:37:01 -0600

    ADDDDDD

--- TravelJunkie/AppDelegate.swift
+++ TravelJunkie/AppDelegate.swift
@@ -8,15 +8,34 @@
 
 import UIKit
 import CoreData
-
+import SwiftyDropbox
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+       
+        if DBSession.shared().handleOpen(url) {
+            if DBSession.shared().isLinked() {
+                NotificationCenter.default.post(name: .init("didLinkToDropboxAccountNotification"), object: nil)
+                
+                return true
+            }
+        }
+        
+        return true
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+         DropboxClientsManager.setupWithAppKey("wfaujeebmsgjlzo")
+        // Override point for customization after application launch.let appKey = "wfaujeebmsgjlzo"      // Set your own app key value here.
+        let appKey = "wfaujeebmsgjlzo"   
+        let appSecret = "hiic5fscfok9wzc"
+        let dropboxSession = DBSession(appKey: appKey, appSecret: appSecret, root: kDBRootAppFolder)
+        DBSession.setShared(dropboxSession)
+        
         return true
     }
 
